a3307c2f971d98dbcc90dcaf1687d0f463224fdc
tian zhang <279736390@qq.com>
2018-04-30 00:25:16 +0800

    Update configuration file

--- src/main/java/com/frame/silly/AutoApplication.java
+++ src/main/java/com/frame/silly/AutoApplication.java
@@ -8,23 +8,25 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication
+//@SpringBootApplication
 public class AutoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AutoApplication.class, args);
+//		SpringApplication.run(AutoApplication.class, args);
+
+		System.out.println("aaa");
 	}
 
-	@Bean
-	public CommandLineRunner runner(PageRepository repository) {
-		return new CommandLineRunner() {
-
-			@Override
-			public void run(String... args) throws Exception {
-//                repository.save(new Page("aaa"));
-				System.err.println(repository.findAll());
-			}
-
-		};
-	}
+//	@Bean
+//	public CommandLineRunner runner(PageRepository repository) {
+//		return new CommandLineRunner() {
+//
+//			@Override
+//			public void run(String... args) throws Exception {
+////                repository.save(new Page("aaa"));
+//				System.err.println(repository.findAll());
+//			}
+//
+//		};
+//	}
 }
